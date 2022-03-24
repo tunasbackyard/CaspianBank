33e76db74dc100fd7813e9236a0cba90fed7b5bb
neglux <akcaaytuna@gmail.com>
2022-03-24 16:39:38 +0300

    adding navbar hover

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,10 +4,50 @@ const DOM = {
   getElements() {
     this.toggleBtn = document.querySelector("#toggle-btn");
     this.navigationMenu = document.querySelector(".navigation");
+    this.navbar = document.querySelector(".primary-navigation");
+    this.logo = document.querySelector(".logo");
+    this.navLinks = document.querySelectorAll(".link");
   },
 };
 
 DOM.getElements();
+
+DOM.navbar.addEventListener("mouseover", handleNavLinkAnimation.bind(0.5));
+DOM.navbar.addEventListener("mouseout", handleNavLinkAnimation.bind(1));
+
+function handleNavLinkAnimation(e) {
+  if (isNavbarLink(e, "link")) {
+    DOM.navLinks.forEach((link) => {
+      if (!isCurrentlyHovered(e, link)) {
+        setOpacity(link, this);
+      }
+    });
+    setOpacity(DOM.logo, this);
+    setOpacity(DOM.toggleBtn, this);
+  }
+}
+
+function isNavbarLink(event, className) {
+  if (event.target.classList.contains(className)) {
+    return true;
+  }
+  return false;
+}
+
+function getCurrentlyHovered(event) {
+  return event.target;
+}
+
+function isCurrentlyHovered(event, element) {
+  if (element === getCurrentlyHovered(event)) {
+    return true;
+  }
+  return false;
+}
+
+function setOpacity(element, level) {
+  element.style.opacity = level;
+}
 
 DOM.toggleBtn.addEventListener("click", function () {
   changeDisplay(DOM.navigationMenu);
